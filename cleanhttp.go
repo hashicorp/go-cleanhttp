be9fcbf5a841c6a0e3c45bea3bb921df3b4f996d
sonnemusk <sonnemusk@gmail.com>
2026-07-22 16:06:35 +0800

    fix: drop deprecated net.Dialer.DualStack setting
    DualStack has had no effect since Go 1.20 (Fast Fallback is always on).
    Remove the dead field from DefaultPooledTransport to avoid noise when
    building with -tags=deprecated or static analysis.

--- cleanhttp.go
+++ cleanhttp.go
@@ -29,7 +29,6 @@ func DefaultPooledTransport() *http.Transport {
 		DialContext: (&net.Dialer{
 			Timeout:   30 * time.Second,
 			KeepAlive: 30 * time.Second,
-			DualStack: true,
 		}).DialContext,
 		MaxIdleConns:          100,
 		IdleConnTimeout:       90 * time.Second,
